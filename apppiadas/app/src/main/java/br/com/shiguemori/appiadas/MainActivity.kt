760a4b87492c47446ad6ed4e82e5f31a8cf7dd51
Vinicius Shiguemori Shirakawabe <shiguemori@MacBook-Pro-de-Vinicius.local>
2022-03-16 22:43:46 -0300

    finish app and i18n

--- apppiadas/app/src/main/java/br/com/shiguemori/appiadas/MainActivity.kt
+++ apppiadas/app/src/main/java/br/com/shiguemori/appiadas/MainActivity.kt
@@ -1,11 +1,39 @@
 package br.com.shiguemori.appiadas
 
-import androidx.appcompat.app.AppCompatActivity
+import android.media.MediaPlayer
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import androidx.appcompat.app.AppCompatActivity
+import br.com.shiguemori.appiadas.databinding.ActivityMainBinding
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        showJoker()
+        binding.btTellJoker.setOnClickListener {
+            binding.btTellJoker.isEnabled = false
+            showJoker()
+            playSong()
+            Handler(Looper.getMainLooper()).postDelayed({
+                binding.btTellJoker.isEnabled = true
+            }, 3000)
+        }
+    }
+
+    private fun playSong() {
+        val mediaPlayer = MediaPlayer.create(this, R.raw.badumtss)
+        mediaPlayer.start()
+    }
+
+    fun showJoker() {
+        val jokers = resources.getStringArray(R.array.jokers)
+        val numberJoker = Random().nextInt(jokers.size)
+        val joker = jokers[numberJoker]
+        binding.tvJoker.text = joker
     }
 }
